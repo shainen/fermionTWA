dfe7483fd2a467bd81f743cb823927b9a012f239
Shainen Davidson <shainen@gmail.com>
2016-03-18 14:17:01 -0400

    Adding qsub maker

---
+++ makeqsubs.py
@@ -0,0 +1,38 @@
+from string import Template
+from os import getcwd
+
+project=getcwd().split('/')[-1]
+
+runname=getcwd().split('/')[-2]
+
+qsubfile = Template("""
+#!/bin/sh
+#PBS -j oe
+#PBS -l mem=300mb
+#PBS -V
+#PBS -m ea
+#PBS -M shainen@gmail.com
+
+RUN_NAME=${rname}
+SCRATCH_DIR=/data/$$USER/$$RUN_NAME/r$$PBS_ARRAYID
+LOCAL_DIR=/home/shainen/hubbard/build
+
+mkdir -p $$SCRATCH_DIR
+
+# Copy them from this directory to data director
+cd $$LOCAL_DIR/$$RUN_NAME
+cp -r ${prj} $$SCRATCH_DIR/
+
+# Move to the data directory
+cd $$SCRATCH_DIR/
+
+# Run the script
+time math -script ${prj}/runTWA.wl
+
+# Remove the now-useless files
+rm -r ${prj} 
+""")
+
+with open("../"+runname+".qsub", "w") as f:
+    f.write(qsubfile.substitute(rname=runname,prj=project))
+
