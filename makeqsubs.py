86059e3e6f99c647850675243eb8b3584cc9adbc
Shainen Davidson <shainen@gmail.com>
2017-01-11 14:16:06 -0500

    start nn s8s cluster

--- makeqsubs.py
+++ makeqsubs.py
@@ -33,7 +33,7 @@ cd ${prj}
 echo "SeedRandom[$$PBS_ARRAYID]" > randomSeed.wl
 cd ..
 # time math -script ${prj}/runTCMBL2d.wl
-time math -script ${prj}/runFermiHubbardBig.wl
+time math -script ${prj}/startFermiHubbardBig.wl
 
 # Remove the now-useless files
 rm -r ${prj} 
